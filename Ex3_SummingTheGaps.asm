38fff24afc6ef481439f13d1d6199f8ef29162c6
Brent Zerbe <zerbzman@gmail.com>
2015-10-14 18:38:51 -0400

    Done
    Couldn’t figure out the pointer on the second assignment.

--- Ex3_SummingTheGaps.asm
+++ Ex3_SummingTheGaps.asm
@@ -8,15 +8,20 @@ ExitProcess proto,dwExitCode:dword
 .data
 dwarray dword  0,2,5,9,10
 count EQU (LENGTHOF dwarray)
+total dword 0
 
 .code
 main proc
-	mov  edi,OFFSET dwarray		; 1: EDI = address of dwarray
-	mov  ecx,LENGTHOF dwarray		; 2: initialize loop counter
-	mov  eax,0
+	mov  edi,OFFSET dwarray		; EDI = address of dwarray
+	mov  ecx,count-1			; initialize loop counter
 
-LoopStart:					;Loop throught the array
-	sub ;Subtract the value from the next value in the array
+LoopStart:						; Loop through the array
+	mov  ebx, [edi]				; get first element
+	add  edi, TYPE dwarray		; increment the address
+	mov  edx, [edi]				; get the next element
+	sub  edx, ebx				; edx = edx - ebx
+	add  total, edx				; total = total + edx
+	loop LoopStart				; repeat until ECX = 0
 
 	invoke ExitProcess,0
 main endp
--- Ex7_CopyStringReverse.asm
+++ Ex7_CopyStringReverse.asm
@@ -11,14 +11,17 @@ target  BYTE  SIZEOF source DUP('#')
 
 .code
 main PROC
-; Point ESI to the last character in the source string:
+mov  esi,0				; index register
+mov  ecx,SIZEOF source	; loop counter
+dec  ecx
+L1:
+	mov  al,source[esi]		; get a character from source
+	mov target[ecx],al		; store it in the target
+	inc  esi				; move to next character
+	loop L1					; repeat for entire string
 
-; Point EDI to the beginning of the target string:
-; We do not copy the null terminator byte.
-
-; Your code goes here	
-
-	mov	BYTE PTR [edi],0			; add a null byte to the target
+;couldn't get this next line to work
+mov	BYTE PTR [edi],0			; add a null byte to the target
 
 	Invoke ExitProcess,0
 main ENDP
